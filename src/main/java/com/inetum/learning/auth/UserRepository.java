201f16ecb025e30497f9b9b35595c262e2957961
niama315 <aroubiteniama@gmail.com>
2022-08-28 12:31:18 +0100

    add model

--- src/main/java/com/inetum/learning/auth/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inetum.learning.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
--- src/main/java/com/inetum/learning/repositories/ProfesseurRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inetum.learning.repositories;
-
-import com.inetum.learning.model.Professeur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesseurRepository extends JpaRepository<Professeur, Long> {
-}
